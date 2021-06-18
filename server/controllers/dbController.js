c1ef3846b5cfd43b058b4ba40df2093e74250d62
vd89 <dixitvora@gmail.com>
2021-06-18 09:31:31 +0530

    alpha: added the change

--- server/controllers/dbController.js
+++ server/controllers/dbController.js
@@ -1,23 +1,23 @@
 import { connect } from 'mongoose';
-import config from '../../config/config';
+import config from '../../config/config.js';
 
 const mongoOpt = {
- keepAlive: true,
- useNewUrlParser: true,
- useUnifiedTopology: true,
- useCreateIndex: true,
- useFindAndModify: false,
- serverSelectionTimeoutMS: 50000,
- socketTimeoutMS: 50000,
+  keepAlive: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  serverSelectionTimeoutMS: 50000,
+  socketTimeoutMS: 50000,
 };
 const { mongoUri } = config;
 
 const dbController = async () => {
- try {
-  await connect(mongoUri, mongoOpt);
-  console.info(`MongoDb -> Connected to mongoDb Server`);
- } catch (err) {
-  console.error('MongoDb ->', err.message);
- }
+  try {
+    await connect(mongoUri, mongoOpt);
+    console.info(`MongoDb -> Connected to mongoDb Server`);
+  } catch (err) {
+    console.error('MongoDb ->', err.message);
+  }
 };
 export default dbController;
