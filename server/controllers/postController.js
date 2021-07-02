402783f0cff49a71cd3db661094d2a2a1abda937
vd89 <dixitvora@gmail.com>
2021-07-02 14:26:01 +0530

    vd89Alpha: added the postbyid and userbyid params and contorllers

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,10 +1,37 @@
+import postModel from '../model/postModel';
 
-export const testPost = (req,res,next) =>{
-try {
- const testPostData = 'The test Post is working '
- return res.ok({message:"SUCCESS", value:testPostData})
- } catch (e) {
-  console.error(e.message);
-  next(e)
- }
-}
+export const testPost = (req, res, next) => {
+	try {
+		const testPostData = 'The test Post is working ';
+		return res.ok({ message: 'SUCCESS', value: testPostData });
+	} catch (e) {
+		console.error(e.message);
+		next(e);
+	}
+};
+
+export const postByID = async (req, res, next, postId) => {
+	try {
+		let post = await postModel.findById(postId).populate('postedBy', '_id userName').exec();
+		if (!post) {
+			return res.error('POST_NOT_FOUND');
+		}
+		req.post = post;
+		next();
+	} catch (e) {
+		console.error(e.message);
+		next(e);
+	}
+};
+
+export const createPost = async (req, res, next) => {
+	try {
+		const { profile } = req;
+		console.log(profile);
+
+		return res.ok({ message: 'SUCCESS' });
+	} catch (e) {
+		console.error(e.message);
+		next(e);
+	}
+};
