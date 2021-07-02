868cc51c65d265b754e4e24d6af7c1b488d993f7
vd89 <dixitvora@gmail.com>
2021-07-02 15:43:43 +0530

    vd89Alpha: added the input validation ruled for new post

--- server/helper/inputValidationHelper.js
+++ server/helper/inputValidationHelper.js
@@ -1,15 +1,21 @@
-import { body,validationResult } from 'express-validator';
+import { body, validationResult } from 'express-validator';
 
+export const inputPostRules = () => [
+	body('tages', 'Tages are require').not().isEmpty(),
+	body('postText', 'Text is required to determine your idea').not().isEmpty(),
+	body('tages', 'Tages should not be empty').isArray({ min: 1 }),
+	body('tages.*', 'Tages langth should be in rage of 4 to  100 character').isLength({ min: 4, max: 100 }),
+	body('postText', 'Text langth should be in rage of 30 to  1000 character').isLength({ min: 30, max: 1000 }),
+];
 
-export const validate = (req,res,next) => {
-const errors = validationResult(req)
+export const validate = (req, res, next) => {
+	const errors = validationResult(req);
 
-if (errors.isEmpty())  return next()
+	if (errors.isEmpty()) return next();
 
-const extractedErrors = []
-errors.array().map(err => {
- extractedErrors.push({[err.location]: err.location, params: err.param, msg:err.msg})
-})
-return res.status(400).json({errors: extractedErrors})
-
-}
+	const extractedErrors = [];
+	errors.array().map((err) => {
+		extractedErrors.push({ [err.location]: err.location, params: err.param, msg: err.msg });
+	});
+	return res.status(400).json({ errors: extractedErrors });
+};
--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { requireSignin } from '../controllers/authController';
 import { createPost, postByID, testPost } from '../controllers/postController';
 import { userByID } from '../controllers/userContoller';
+import { inputPostRules, validate } from '../helper/inputValidationHelper';
 
 const postRouter = new Router();
 
@@ -9,7 +10,7 @@ const postRouter = new Router();
 postRouter.get('/', requireSignin, testPost);
 
 // create new post
-postRouter.post('/create/:userId', requireSignin, createPost);
+postRouter.post('/create/:userId', requireSignin, inputPostRules(), validate, createPost);
 
 // Params route
 postRouter.param('userId', userByID);
