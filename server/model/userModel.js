b16b9bf64df3bd5c0b50c7671e0bbcfb7b35f2de
vd89 <dixitvora@gmail.com>
2021-07-02 14:26:01 +0530

    vd89Alpha: added the postbyid and userbyid params and contorllers

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -1,25 +1,27 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model, Types } from 'mongoose';
+import { generateRandomSring } from '../helper/encriptionHelper';
 
 const FROM_USER = {
- GOOGLE: 'google',
- FACEBOOK: 'facebook',
- DEFAULT: null,
+	GOOGLE: 'google',
+	FACEBOOK: 'facebook',
+	DEFAULT: null,
 };
 
 const userSchema = new Schema(
- {
-  userName: { type: String },
-  email: { type: String },
-  picture: { type: String },
-  userFrom: { type: String, enum: Object.values(FROM_USER), default: FROM_USER.DEFAULT },
-  location: {
-   type: { type: String,  default: 'Point' },
-   coordinates: { type: [Number] },
-  },
-  following: [{ type: Schema.ObjectId, ref: 'User' }],
-  followers: [{ type: Schema.ObjectId, ref: 'User' }],
- },
- { timestamps: true }
+	{
+		_id: { type: String, default: () => `padosiUser-${Date.now()}${generateRandomSring(12)}` },
+		userName: { type: String },
+		email: { type: String },
+		picture: { type: String },
+		userFrom: { type: String, enum: Object.values(FROM_USER), default: FROM_USER.DEFAULT },
+		location: {
+			type: { type: String, default: 'Point' },
+			coordinates: { type: [Number] },
+		},
+		following: [{ type: Schema.ObjectId, ref: 'User' }],
+		followers: [{ type: Schema.ObjectId, ref: 'User' }],
+	},
+	{ timestamps: true },
 );
 
 export default model('User', userSchema);
