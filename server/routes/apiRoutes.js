45ed32950bdc119e08d13d42b4d453694910b134
daxMarktine <dixit.vora@marktine.com>
2021-07-13 11:32:22 +0530

    vd89Alpha: added the test for user register and login

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -3,18 +3,18 @@ import authRoute from './authRoute';
 import postRouter from './postRoute';
 import userRouter from './userRoute';
 
-import {  testAuth } from '../controllers/authController';
+import { testAuth } from '../controllers/authController';
 
 const apiRoutes = new Router();
 
 /*
   test the api route
 */
-apiRoutes.get('/',testAuth)
+apiRoutes.get('/', testAuth);
 
 apiRoutes.use('/auth', authRoute);
 // api/post/
 apiRoutes.use('/post', postRouter);
-apiRoutes.use('/user', userRouter)
+apiRoutes.use('/user', userRouter);
 
 export default apiRoutes;
---
+++ server/test/outputData.js
@@ -0,0 +1,23 @@
+export const userOutputData = {
+	user: {
+		location: {
+			type: 'Point',
+			coordinates: [70.765, 22.3016],
+		},
+		bio: null,
+		userFrom: 'google',
+		distance: 5000,
+		following: [],
+		followers: [],
+		_id: '60ec1a4adf3b508d96bff2ae',
+		name: 'Dixit Vora',
+		email: 'dixitvora2020@gmail.com',
+		picture: 'https://robohash.org/1PU.png?set=set2&size=150x150',
+		userId: 'padosiUser-16260859625249c3ed91b0c25',
+		createdAt: '2021-07-12T10:32:42.528Z',
+		updatedAt: '2021-07-12T10:32:42.528Z',
+		__v: 0,
+	},
+	token:
+		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJwYWRvc2lVc2VyLTE2MjYwODU5NjI1MjQ5YzNlZDkxYjBjMjUiLCJpYXQiOjE2MjYxNTE1MzJ9.pFivQ85vkNR60t1N188QH2csN1_NMTszdK5egToCq6w',
+};
