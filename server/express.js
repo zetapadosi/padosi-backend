8addf6a9985666706ffaeb6300f7fc3ce257e7ba
vd89 <dixitvora@gmail.com>
2021-07-16 13:09:23 +0530

    vd89Alpha: updated in cors

--- server/express.js
+++ server/express.js
@@ -28,17 +28,6 @@ i18n.configure({
 import './model';
 // Middleware
 
-var allowlist = ['http://localhost:3000'];
-var corsOptionsDelegate = function (req, callback) {
-	var corsOptions;
-	if (allowlist.indexOf(req.header('Origin')) !== -1) {
-		corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-	} else {
-		corsOptions = { origin: false }; // disable CORS for this request
-	}
-	callback(null, corsOptions); // callback expects two parameters: error and options
-};
-
 app.set('trust proxy', 1);
 app.use(i18n.init);
 app.use(cookieParser());
@@ -58,7 +47,13 @@ app.use(serveFavicon('./source/nodejs.svg'));
 app.use(morgan('combined'));
 app.use(compression());
 app.use(helmet());
-app.use(cors(corsOptionsDelegate));
+app.use(
+	cors({
+		origin: 'http://localhost:3000',
+		credentials: true,
+		optionsSuccessStatus: 200,
+	}),
+);
 
 // check the cookie and remove if is not set
 app.use(sessionCler);
